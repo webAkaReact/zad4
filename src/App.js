dd335831501fe81607ed709d2aa71ccbeae8fb09
Marcin Cielecki <m.cielecki@ideo.pl>
2020-10-24 19:51:19 +0200

    'zad4'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import img from './img.PNG'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    quotes: [
+        {
+        "quote": "Genius is one percent inspiration and ninety-nine percent perspiration.",
+        "author": "Thomas Edison"
+        },
+        {
+        "quote": "You can observe a lot just by watching.",
+        "author": "Yogi Berra"
+        },
+        {
+        "quote": "A house divided against itself cannot stand.",
+        "author": "Abraham Lincoln"
+        },
+        {
+        "quote": "Difficulties increase the nearer we get to the goal.",
+        "author": "Johann Wolfgang von Goethe"
+        },
+        {
+        "quote": "Fate is in your hands and no one elses",
+        "author": "Byron Pulsifer"
+        },
+        {
+        "quote": "Be the chief but never the lord.",
+        "author": "Lao Tzu"
+      }
+    ]
+  }
+  render()
+  {
+    // 1. zbuduj strukture w html ktora bedzie zawierala boxa w ktorym umiescisz cytat oraz autora a ponizej przycisk 
+    // 2. stworz metode ktora podepniesz pod zdarzenie onClick na przycisk
+    // 3. metoda ta ma losować losowy cytat z tablicy quotes i go wyświetlać
+    return (
+      <div className="App">
+        <img src={img} alt="" />
+      </div>
+    );
+  }
 }
 
 export default App;
